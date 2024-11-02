47ae09dfb34851e54af3d29053a344cbf6916ac5
ahana-sarkar2011 <ulyssesroad30@gmail.com>
2024-11-02 09:42:19 +0000

    Add files via upload

--- game.js
+++ game.js
@@ -4,6 +4,12 @@ AFRAME.registerComponent("gamerules",{
        this.el.addEventListener("collide",function(e){
         turtlenumber=turtlenumber-1
         e.detail.target.el.remove();
+        turtlenumbertext=document.getElementById("text1")
+        turtlenumbertext.setAttribute("text",{value:String(turtlenumber)})
+        if (turtlenumber==0){
+            winnersign=document.getElementById("text2")
+            winnersign.setAttribute("text", {value:"You Win"})
+        }
        }) 
     }
 })
